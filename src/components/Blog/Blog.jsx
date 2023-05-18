1d067765af19fbf616f95ac5c46e6e2b3d50e1c5
Naimur Rahman <nirzon.naim@gmail.com>
2023-05-18 07:51:37 -0400

    Completed Blog Section

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,86 @@
-
+import "./Blog.css";
 
 const Blog = () => {
     return (
-        <div>
-            <h1>This is blog page</h1>
+        <div className="container">
+            <h3 className="fw-bold">1. What is an access token and refresh token? How do they work and where should we store them on the client-side?</h3> <br />
+
+            <p className="justify">An access token is a credential that is issued to a client (e.g., a user or an application) after a successful authentication. It is a string of characters that represents the authorization granted to the client to access certain resources or perform specific actions on behalf of the authenticated user. The access token is typically short-lived and has an expiration time, after which it becomes invalid. <br /> <br />
+                Refresh tokens, on the other hand, are long-lived credentials that are also issued to the client during the authentication process. Their purpose is to obtain a new access token once the current access token expires. Refresh tokens are securely stored and used to request a new access token without requiring the user to re-enter their credentials. They provide a convenient way to maintain a user's session without frequent authentication. <br /> <br />
+                It's important to store access tokens and refresh tokens securely on the client-side to prevent unauthorized access. Common storage options include secure HTTP-only cookies, browser's local storage, or in-memory storage. Each option has its own considerations in terms of security, manageability, and compatibility with the specific application requirements. It's recommended to follow security best practices and consider factors such as token expiration, token revocation, and protection against cross-site scripting (XSS) attacks.
+            </p> <br /> <br />
+
+
+
+            <h3 className="fw-bold">2. Compare SQL and NoSQL databases?</h3> <br />
+
+            <p className="justify">SQL and NoSQL are two different types of database management systems, each with its own characteristics and use cases. Here's a comparison between SQL and NoSQL databases: <br /> <br />
+                SQL Databases: <br /> 
+                
+                <ul>
+                        <li>
+                        Structured Query Language (SQL) databases are based on a relational model.
+                        </li>
+                        <li>
+                        SQL databases use tables with predefined schemas to store and organize data.
+                        </li>
+
+                        <li>
+                        Data in SQL databases follows a rigid structure with fixed columns and data types.
+                    </li>
+                    
+                    <li>
+                    SQL databases ensure data integrity through ACID (Atomicity, Consistency, Isolation, Durability) properties.
+                    </li>
+                    
+                    <li>
+                    Examples of SQL databases include MySQL, PostgreSQL, Oracle, and SQL Server.
+                        </li>
+                    </ul> 
+                NoSQL Databases: <br />
+                <ul>
+                        <li>
+                        NoSQL databases are non-relational and do not use SQL as a query language.
+                        </li>
+                        <li>
+                        NoSQL databases are designed for flexible and unstructured data storage.
+                        </li>
+
+                        <li>
+                        Data in NoSQL databases can be stored in various formats like key-value pairs, documents, graphs, or wide-column stores.
+                    </li>
+                    
+                    <li>
+                    NoSQL databases are suitable for handling big data, real-time applications, and scenarios with rapidly changing data.
+                    </li>
+                    
+                    <li>
+                    Examples of NoSQL databases include MongoDB, Cassandra, Redis, and Neo4j.
+                        </li>
+                    </ul> 
+            </p> <br /> <br />
+
+
+
+            <h3 className="fw-bold">3. What is express js? What is Nest JS?</h3> <br />
+
+            <p className="justify">Express.js is a popular web application framework for Node.js. It provides a robust set of features and tools to build web applications and APIs. Express.js is known for its simplicity, minimalism, and flexibility, allowing developers to create server-side applications quickly and efficiently. It provides routing capabilities, middleware support, template engines for rendering views, and integrates well with various databases and other Node.js modules. <br /> <br />
+            NestJS, on the other hand, is a progressive Node.js framework for building efficient, scalable, and maintainable server-side applications. NestJS is built on top of Express.js and provides an opinionated structure and architecture inspired by Angular, making it ideal for building enterprise-level applications. It emphasizes modularity, dependency injection, and the use of decorators to define components, controllers, and modules. NestJS also provides features like built-in support for TypeScript, powerful routing, middleware, and an extensible ecosystem of modules. <br /> <br />
+                
+            </p> <br /> <br />
+
+
+            <h3 className="fw-bold">4. What is MongoDB aggregate and how does it work?</h3> <br />
+
+<p className="justify">In MongoDB, the aggregate function is used to perform advanced data aggregation operations on collections. It allows you to process and transform data using a pipeline of stages. The aggregate function takes an array of stages as input, where each stage represents a specific operation in the aggregation pipeline.<br /> <br />
+                The aggregation pipeline consists of multiple stages, and each stage performs a specific operation on the input data and passes the result to the next stage. The stages can include operations like filtering documents, grouping documents based on specific criteria, performing calculations, sorting, and more. The output of one stage becomes the input for the next stage, allowing you to create complex data transformation pipelines. <br /> <br />
+                Each stage in the aggregation pipeline is represented by an object with a specific operator. Some common operators used in the aggregation pipeline include $match, $group, $project, $sort, $limit, $skip, and $lookup, among others. These operators enable you to perform operations like filtering, grouping, projecting specific fields, sorting, limiting results, joining collections, and performing various calculations.
+    
+</p> <br /> <br />
+
+
+            
+            
         </div>
     );
 };
