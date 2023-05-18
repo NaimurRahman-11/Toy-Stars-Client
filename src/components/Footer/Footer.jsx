3e40a4f1ce8b2f778d1c778af882830adb1b0884
Naimur Rahman <nirzon.naim@gmail.com>
2023-05-18 02:10:01 -0400

    Completed Navbar and Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,71 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaPinterest, FaTwitter } from 'react-icons/fa';
+import "./Footer.css"
 
 const Footer = () => {
     return (
         <div>
-            <h1>This is Footer</h1>
+            <footer className="text-center">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-4 mb-5">
+                        <img src="https://i.ibb.co/bmn3yVK/logo.png" className='logo' alt="" /> <br />
+                            <p>
+                                ToyStars is an online store for attractive Action Figure Toys! Browse our most beautiful toys and pick one for you and your lovable one.
+                            </p>
+                        </div>
+
+                        <div className="col-lg-4 mb-5">
+                            <h4>Contact Us</h4>
+                            <p>
+                                Email: toy@toystars.com <br />
+                                Phone: 123-456-7890 <br />
+                                Address: 123 Main St, Anytown USA
+                            </p>
+                        </div>
+
+                        <div className="col-lg-4">
+                            <h4>Follow Us</h4>
+                            <ul className="list-inline">
+                                <li className="list-inline-item">
+                                    <a href="#">
+                                        <FaFacebook></FaFacebook>
+                                    </a>
+                                </li>
+                                <li className="list-inline-item">
+                                    <a href="#">
+                                        <FaTwitter></FaTwitter>
+                                    </a>
+                                </li>
+                                <li className="list-inline-item">
+                                    <a href="#">
+                                        <FaInstagram></FaInstagram>
+                                    </a>
+                                </li>
+
+                                <li className="list-inline-item">
+                                    <a href="#">
+                                        <FaLinkedin></FaLinkedin>
+                                    </a>
+                                </li>
+
+                                <li className="list-inline-item">
+                                    <a href="#">
+                                        <FaPinterest></FaPinterest>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-lg-12 mt-5">
+                            <p>
+                                &copy; 2023 Toy Stars. All Rights Reserved.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,34 +6,37 @@ const Navbar = () => {
     return (
         <div>
             <div>
-            <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <Link to="/" className="navbar-brand"><img src="https://images-platform.99static.com//jZDMjog_ehY68oQ1vbDGVMSrebY=/17x15:945x943/fit-in/500x500/99designs-contests-attachments/62/62129/attachment_62129397" alt="" className='logo' /></Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav p-2 mx-auto mb-2 mb-lg-0">
-                            <li className='nav-item me-3'>
-                                <Link to='/' className="nav-link myColor">Home</Link>
-                            </li>
-                            <li className='nav-item me-3'>
-                                <Link to='/blog' className="nav-link myColor">Blog</Link>
-                            </li>
-                            
-                        </ul>
-                        <div>
-                        
-   
+                <nav className="navbar navbar-expand-lg">
+                    <div className="container-fluid">
+                        <Link to="/" className="navbar-brand"><img src="https://i.ibb.co/bmn3yVK/logo.png" alt="" className='logo' /></Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav p-2 mx-auto mb-2 mb-lg-0">
+                                <li className='nav-item me-3'>
+                                    <Link to='/' className="nav-link myColor">Home</Link>
+                                </li>
+                                <li className='nav-item me-3'>
+                                    <Link to='/blog' className="nav-link myColor">Blog</Link>
+                                </li>
+                                <li className='nav-item me-3'>
+                                    <Link to='/blog' className="nav-link myColor">All Toys</Link>
+                                </li>
 
-                        </div>
-                        
-                           
+                            </ul>
+                            <div>
+
+
+
+                            </div>
+
+
                             <Link to="/login"><button className="btn btn-outline-warning" type="submit">Login</button></Link>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </div>
+                </nav>
+            </div>
         </div>
     );
 };
