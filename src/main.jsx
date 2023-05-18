6061d18addd07c2be329740b69c442099eed1aaf
Naimur Rahman <nirzon.naim@gmail.com>
2023-05-18 01:31:45 -0400

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Layout/Main.jsx';
+import Login from './components/Login/Login.jsx';
+import Blog from './components/Blog/Blog';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
